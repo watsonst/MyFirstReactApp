433f1f78f9f979245e1190beb667bcfe463630d3
Stephanie <me@stephaniemariewatson.com>
2023-04-06 15:00:02 -0400

    Add notes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <div>Hello!</div>;
+  return <div>Hello!</div>;//returns jsx code(so html code in js)
 }
 
-export default App;
+export default App;//app is a react compmonent, can be used as html element as in index.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; //object created by react
 
 import './index.css';
-import App from './App';
+import App from './App';//extensions can excluded for js files
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root')); //<App /> is an example of jsx syntax, can't just write html in js files. "root" defined in public>index.html
