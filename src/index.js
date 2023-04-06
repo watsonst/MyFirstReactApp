5569c178e2697dcafabd0f7684bcef8c87887466
Stephanie <me@stephaniemariewatson.com>
2023-04-06 15:28:01 -0400

    Add Todo as a component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; //object created by react
+import ReactDOM from 'react-dom/client'; //object created by react
 
 import './index.css';
 import App from './App';//extensions can excluded for js files
 
 
-ReactDOM.render(<App />, document.getElementById('root')); //<App /> is an example of jsx syntax, can't just write html in js files. "root" defined in public>index.html
+const root = ReactDOM.createRoot(document.getElementById('root')); //<App /> is an example of jsx syntax, can't just write html in js files. "root" defined in public>index.html
+root.render(<App />);
