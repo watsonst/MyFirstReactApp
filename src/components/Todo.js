2a7e89e07c9665136cc1b11c0383e4ad71a5f3e6
Stephanie <me@stephaniemariewatson.com>
2023-04-07 10:06:01 -0400

    Add scaffolding for deleteHandler

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,19 @@
 function Todo(props) {
+  function deleteHandler(){
+      console.log('Clicked!');
+      console.log(props.text);
+  }
+
+
   return (
-    <div className="card">
+    <div className='card'>
       <h2>{props.text}</h2>
-      <div className="actions">
-        <button className="btn">Delete</button>
+      <div className='actions'>
+        <button className='btn' onClick={deleteHandler}>Delete</button> 
       </div>
     </div>
   );
 }
 
 export default Todo; //Have to export it to be usuable by other components. Start function name with Capital
+//deleteHandler is NOT executed on line 9, no (). Just points to function. Only want to execute function when button is clicked, not when js evaluates the code at the start
